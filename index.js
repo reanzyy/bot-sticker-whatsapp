b5d39180bf54bc74e402924a532df457a9f2fcc6
Adriansyah Suryawan <suryawanadriansyah4@gmail.com>
2024-03-05 09:14:53 +0700

    first commiit

---
+++ index.js
@@ -0,0 +1,37 @@
+const qrcode = require('qrcode-terminal');
+const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
+
+const client = new Client({
+  authStrategy: new LocalAuth()
+});
+
+client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', async (message) => {
+    if (message.body === '.test') {
+        await client.sendMessage(message.from, 'hii bot is on!');
+    } else if (message.type === 'image' && message.body.startsWith('.sticker')) {
+        const media = await message.downloadMedia();
+        client.sendMessage(message.from, media, {
+            sendMediaAsSticker: true
+        });
+    } else if (message.body === '.random') {
+        const random = await MessageMedia.fromUrl('https://picsum.photos/200', { unsafeMime: true });
+        await client.sendMessage(message.from, random);
+    } else if (message.body.lastIndexOf && message.body === '.download') {
+        if (message.hasMedia) {
+            const media = await message.downloadMedia();
+            client.sendMessage(message.from, media, { caption: 'Here is your downloaded message:' });
+        } else {
+            client.sendMessage(message.from, message.body.lastIndexOf);
+        }
+    }
+});
+
+client.initialize();
